4cf37a7b1cc829b1120d652ecc3dd375d44115d7
szymel2020 <szymon.chala@gmail.com>
2021-06-25 22:25:49 +0200

    getElementById

--- kalkulator.js
+++ kalkulator.js
@@ -1,4 +1,9 @@
 document.getElementById("n0").addEventListener("click", function () {
     
-    document.getElementById("xc").innerHTML = "ngfd";
+    document.getElementById("box1").innerHTML = "0";
+})
+
+document.getElementById("n1").addEventListener("click", function () {
+    
+    document.getElementById("box1").innerHTML = "1";
 })
