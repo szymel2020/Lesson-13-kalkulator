ffb7d735538c6042611a0e1272439a3ef351bb0a
szymel2020 <szymon.chala@gmail.com>
2021-06-26 14:29:21 +0200

    average

--- kalkulator.js
+++ kalkulator.js
@@ -1,7 +1,17 @@
+
+
 document.getElementById("n0").addEventListener("click", function () {
      var box1 = parseInt(document.getElementById("box1").value);
      var box2 = parseInt(document.getElementById("box2").value);
      var rezultat = box1 + box2;
      document.getElementById("box3").value = rezultat;
+})
+
+document.getElementById("n9").addEventListener("click", function(){
+     var box7 = parseInt(document.getElementById("box7").value);
+     var box8 = parseInt(document.getElementById("box8").value);
+     var taSrednia = (box7 + box8) / 2;
+     
+     document.getElementById("box9").value = taSrednia;
 })
 
