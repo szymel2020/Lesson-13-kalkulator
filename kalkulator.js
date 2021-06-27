a2e7cf36750400ddf05b5b8361e0b3cbbd9a9c8b
szymel2020 <szymon.chala@gmail.com>
2021-06-27 23:18:00 +0200

    change add button

--- kalkulator.js
+++ kalkulator.js
@@ -7,11 +7,21 @@ document.getElementById("n0").addEventListener("click", function () {
      document.getElementById("box3").value = rezultat;
 })
 
-document.getElementById("n9").addEventListener("click", function(){
+/* document.getElementById("n9").addEventListener("click", function(){
      var box7 = parseInt(document.getElementById("box7").value);
      var box8 = parseInt(document.getElementById("box8").value);
      var taSrednia = (box7 + box8) / 2;
      
      document.getElementById("box9").value = taSrednia;
 })
+ */
 
+
+/* function () {
+     if (box7.value != "" && box8.value != "") {
+          var box7 = document.getElementById("box7").value;
+          var box8 = document.getElementById("box8").value;
+          var add = box7 + box8;
+          document.getElementById("box9").innerHTML = add;
+     }
+} */
